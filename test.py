1581201d89f01f2b0aa9fb7f02a6f8508026a5a4
AbhiSharma22299 <abhisheksharmabca2020@gmail.com>
2024-01-21 01:51:58 +0530

    P

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("hello world!");
+
+# comments single line
+"""aksnasnk // Multi-Line comments
+adsjknaklsnklasdn"""
+
+# print("hello world!");
+
+print("Abhishek", 10, 6 , sep=" ~ ", end="2002\n")
+print("Hello")  
