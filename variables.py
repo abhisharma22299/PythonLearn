59fe355ea9f74a4070eed4240998ad75cc339cfa
AbhiSharma22299 <abhisheksharmabca2020@gmail.com>
2024-01-27 23:47:27 +0530

    Py

--- variables.py
+++ variables.py
@@ -12,4 +12,14 @@ comm=complex(8,2)
 print(comm)
 # Sequened data type List and tupples
 list=[8,3,-4,-2,["Apple","Bannana"]]
-print(list[0])
+print(list)
+list[1]=10
+print(list[1])
+# Immutalbe Tuples
+tuple=(8,3,-4,-2,("Apple","Bannana"))
+print(tuple)
+# tuple[1]=10
+print(tuple[1])
+# Distonary
+dis={"Name":"Abhishek sharma","Age":20}
+print(dis.Name)
