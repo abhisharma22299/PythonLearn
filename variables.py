a76f33aa9aaf1fa5a664d0948b78971ed8f1ba9e
AbhiSharma22299 <abhisheksharmabca2020@gmail.com>
2024-01-27 23:32:50 +0530

    Py

--- variables.py
+++ variables.py
@@ -6,4 +6,7 @@ print(b)
 print("Hello")
 print("type of b is ",type(b))
 print("Hello " + "World")
-print("Abhishek "+"Sharma")
+print("Abhishek "+"Sharma") 
+# complex number like 2+3i
+comm=complex(8,2)
+print(comm)
